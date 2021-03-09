ee68026c3df57ea24bd10a0ba2edab67a0f76607
Error6251 <juhair98@gmail.com>
2021-03-09 17:56:59 +0600

    added: prismJS and codeCoppy

--- themes/utitled-hugo-doc/assets/js/script.js
+++ themes/utitled-hugo-doc/assets/js/script.js
@@ -1,1 +1,20 @@
-console.log("Untitled");
+var clipboard = new ClipboardJS(".prism-btn", {
+  target: function (trigger) {
+    return trigger.previousElementSibling;
+  },
+});
+
+clipboard.on("success", function (event) {
+  event.clearSelection();
+  event.trigger.textContent = "Copied";
+  window.setTimeout(function () {
+    event.trigger.textContent = "Copy";
+  }, 2000);
+});
+
+copyCode.on("error", function (event) {
+  event.trigger.textContent = 'Press "Ctrl + C" to copy';
+  window.setTimeout(function () {
+    event.trigger.textContent = "Copy";
+  }, 2000);
+});
